4408dd30cb5034af3752b4cc8a6234d4f005b3da
prasad kavinda <pp.kavinda@gmail.com>
2019-03-15 20:08:01 +0530

    add production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: 'AIzaSyBgTnUPGRxLDBY68zg4AdLLw32MBnFVd2E',
+    authDomain: 'practice-4eea4.firebaseapp.com',
+    databaseURL: 'https://practice-4eea4.firebaseio.com',
+    projectId: 'practice-4eea4',
+    storageBucket: 'practice-4eea4.appspot.com',
+    messagingSenderId: '697255292974',
+  },
 };
